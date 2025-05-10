0c0a132606e7cfa5fbabc1fe2f7b9059f963a220
nitish-vashisth <nitishlvashisth@gmail.com>
2025-05-10 12:42:22 +0530

    Adding Read Me

---
+++ complaint-system/src/main/java/service/Driver.java
@@ -0,0 +1,8 @@
+package service;
+
+public class Driver {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Complain System");
+    }
+}
--- payment-complaint-system/src/main/java/service/PaymentController.java
+++
@@ -1,8 +0,0 @@
-package service;
-
-public class PaymentController {
-
-    public static void main(String[] args) {
-        System.out.println("Payment Complain System");
-    }
-}
